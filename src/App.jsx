62a39cfc2700b3f1e3ffe3af3e111109ef64ca41
bruno-guilherme <dasilvaguilhermebruno@gmail.com>
2024-10-02 18:54:43 -0300

    feat: implementacao das rotas principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
 
 function App() {
-
   return (
-    <>
-      <Home />
-      <About />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/users" element={<Users />} /> */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
