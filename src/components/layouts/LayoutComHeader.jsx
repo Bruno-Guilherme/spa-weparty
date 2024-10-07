2149fa11c9b3cbd5f5b25964b703057669050994
bruno-guilherme <dasilvaguilhermebruno@gmail.com>
2024-10-07 17:26:11 -0300

    update: uso do material ui para estilizar o header

--- src/components/layouts/LayoutComHeader.jsx
+++ src/components/layouts/LayoutComHeader.jsx
@@ -1,32 +1,80 @@
 import { Outlet } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  TextField,
+  Button,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const LayoutComHeader = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-      <header>
-        <p>menu hamburguer</p>
-        <h1>WeParty</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">Sobre</a>
-            </li>
-          </ul>
-        </nav>
-        <input type="text" name="inp-buscar" id="inp-buscar" placeholder="pesquisar no WeParty"/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            WeParty
+          </Typography>
+          <TextField
+            variant="outlined"
+            size="small"
+            placeholder="Pesquisar no WeParty"
+            sx={{ marginRight: 2 }}
+          />
+          <Button color="inherit">Entrar</Button>
+          <Button color="inherit">Carrinho</Button>
+        </Toolbar>
+      </AppBar>
 
-        <p>entrar</p>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleMenuClose}>
+          <a href="/">Home</a>
+        </MenuItem>
+        <MenuItem onClick={handleMenuClose}>
+          <a href="/about">Sobre</a>
+        </MenuItem>
+      </Menu>
 
-        <p>carrinho</p>
-      </header>
       <main>
-        <Outlet />
+        <Box sx={{ flexGrow: 1, padding: 2 }}>
+          {" "}
+          {/* Box adicionando padding */}
+          <Outlet />
+        </Box>
       </main>
+
       <footer>
-        <p>© 2024 WeParty</p>
+        <Typography variant="body2" color="text.secondary" align="center">
+          © 2024 WeParty
+        </Typography>
       </footer>
     </>
   );
