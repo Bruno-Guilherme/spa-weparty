7e9814ba5f70e00352e86f778657394cba86e772
Douglas Lima <150302726+DouglasLima-Front@users.noreply.github.com>
2024-10-07 22:29:59 +0000

    alteracaosobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export function About() {
     return (
         <Container maxWidth="md">
             <Box sx={{ padding: 2, textAlign: 'center' }}>
-                <Typography variant="h1" component="h1" gutterBottom>
+                <Typography variant="h2" component="h2" gutterBottom>
                     SOBRE
                 </Typography>
                 <Typography variant="h5" component="h2" gutterBottom>
@@ -17,6 +17,15 @@ export function About() {
                 <Typography variant="body1">
                     Aqui você pode encontrar fornecedores de diversos serviços, desde decoração até catering, tudo em um só lugar. 
                     Navegue pelo nosso site e descubra tudo o que temos a oferecer!
+                </Typography>
+
+                <Typography variant="h5" component={"h5"} gutterBottom>
+                    <br/>
+                    WeParty - A história!
+                </Typography>
+                <Typography variant="body1" textAlign={"center"}> 
+                O WeParty foi criado para facilitar a conexão entre organizadores de eventos e fornecedores de serviços especializados, permitindo que você encontre tudo o que precisa para seu evento em um só lugar. <br/>
+                Ele surgiu a partir de um projeto de Residência Digital, com cinco estudantes, alunos do Embarque Digital no Recife.
                 </Typography>
             </Box>
         </Container>
