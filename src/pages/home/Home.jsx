69ec577343034f221360bd7d08ebdb74cfca1d31
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-10-30 00:24:55 +0000

    feat: implementação do carGrande Promoções da semana

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,12 @@ import imgFotografia from "../../assets/card-pequeno/fotografia.png";
 import imgPersonalizados from "../../assets/card-pequeno/personalizados.png";
 
 import { Box } from "@mui/material";
+import { CardGrande } from "../../shared/components/card-grande/CardGrande";
+import img1 from "../../assets/card-grande/img1.png";
+import img2 from "../../assets/card-grande/img2.png";
+import img3 from "../../assets/card-grande/img3.png";
+import img4 from "../../assets/card-grande/img4.png";
+
 
 export function Home() {
   return (
@@ -28,6 +34,25 @@ export function Home() {
             <CardPequeno categoriaNome={"Personalizados"} urlImagem={imgPersonalizados} descricaoImagem={"Ícone personalizados"} />
             <CardPequeno categoriaNome={"Cerimonial"} urlImagem={imgCerimonial} descricaoImagem={"Ícone cerimonial"} />
             <CardPequeno categoriaNome={"Fotografia"} urlImagem={imgFotografia} descricaoImagem={"Ícone fotografia"} />
+          </Box>
+        </Box>
+        <Box
+        sx={{ textAlign: 'center' }}
+        >
+          <h2 >Promoções da semana</h2>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "row",
+              gap: 2, // Espaçamento entre os itens
+              //flexWrap: "wrap", // Permite que os itens quebrem de linha se a tela for pequena
+            }}          
+          >
+            <CardGrande categoriaNome={"BlackFriday para Buffet"} urlImagem={img1} descricaoImagem={"ícone preto Black Friday"}/>
+            <CardGrande categoriaNome={"50% Off para fotografia"} urlImagem={img2} descricaoImagem={"ícone preto Black Friday"}/>
+            <CardGrande categoriaNome={"Sale em personalizados"} urlImagem={img3} descricaoImagem={"ícone preto Black Friday"}/>
+            <CardGrande categoriaNome={"Black Friday para fotografia"} urlImagem={img4} descricaoImagem={"ícone preto Black Friday"}/>
           </Box>
         </Box>
       </main>
