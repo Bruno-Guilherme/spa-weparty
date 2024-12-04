8c61d627b5cb62bf412ab59cdd6f74528976f912
Bruno Guilherme da Silva <121551639+Bruno-Guilherme@users.noreply.github.com>
2024-12-04 18:59:38 +0000

    refact: layout com header

--- src/shared/components/footer/Footer.jsx
+++ src/shared/components/footer/Footer.jsx
@@ -2,7 +2,14 @@ import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ padding: 2, textAlign: "center" }}>
+    <Box
+      component="footer"
+      sx={{
+        padding: 2,
+        textAlign: "center",
+        boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.1)", // Sombra sutil no topo do footer
+      }}
+    >
       <Typography variant="body2" color="text.secondary">
         © 2024 WeParty
       </Typography>
