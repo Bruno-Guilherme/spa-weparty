b26d8db5617f8b802c34f5910804053207f4afe4
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-12-05 11:57:37 +0000

    alteracao cardMedio

--- src/shared/components/card-medio/CardMedio.jsx
+++ src/shared/components/card-medio/CardMedio.jsx
@@ -1,12 +1,21 @@
-export function CardMedio({ categoriaNome, urlImagem }) {
-    return (
-      <section style={{ textAlign: "center", padding: "16px" }}>
-        <img
-          src={urlImagem}
-          alt={categoriaNome}
-          style={{ width: "100px", height: "100px", borderRadius: "8px" }}
-        />
-        <p>{categoriaNome}</p>
-      </section>
-    );
-  }
+export const CardMedio = ({ categoriaNome, urlImagem, descricaoImagem }) => {
+  return (
+    <section
+      style={{
+        textAlign: "center",
+        padding: "16px",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+        borderRadius: "8px",
+        backgroundColor: "#fff",
+      }}
+    >
+      <img
+        src={urlImagem}
+        alt={categoriaNome}
+        style={{ width: "100px", height: "100px", borderRadius: "8px" }}
+      />
+      <p style={{ fontWeight: "bold", marginTop: "8px" }}>{categoriaNome}</p>
+      <p style={{ color: "gray", fontSize: "0.875rem" }}>{descricaoImagem}</p>
+    </section>
+  );
+};
