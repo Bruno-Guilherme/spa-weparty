b91db837527104c4a24532dafd67f8c22e5ee320
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-12-05 17:10:24 +0000

    estrutura fornecedores

--- src/pages/fornecedores/Fornecedores.jsx
+++ src/pages/fornecedores/Fornecedores.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
-import FornecedorCard from '../fornecedores/FornecedorCard';
+//import FornecedorCard from '../fornecedores/FornecedorCard';
 
+const Fornecedores = () => {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'left' }}>Fornecedores</h1>
+      </div>
+    );
+  };
+  
+  export default Fornecedores;
